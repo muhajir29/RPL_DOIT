f82c4c220126fcc67acea23b8cbc0126c0cde527
muhajir29 <imammuhajir92@gmail.com>
2020-05-11 00:15:08 +0700

    menambahkan feature login dan logout

--- DOIT/urls.py
+++ DOIT/urls.py
@@ -2,11 +2,15 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from . import views
+from .views import index, loginView, logoutView
+
 
 urlpatterns = [
+
+    url(r'^login/$', loginView , name = "login"),
+    url(r'^logout/$', logoutView, name = "logout"),
+    url(r'^$', index,  name = "index"),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
     url(r'^control/', include('control.urls')),
     url(r'^laporandata/', include('laporandata.urls')),
     url(r'^rab/', include('rab.urls')),
--- DOIT/views.py
+++ DOIT/views.py
@@ -1,12 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.views.generic import TemplateView
+
+from django.contrib.auth import authenticate, login, logout
+
 
 def index(request):
-
     context = {
         'judul' : 'DO IT',
         'subjudul' : 'selamat datang di DOIT (DANA OKE INTIME)',
+    }
+    return render(request, 'index.html', context)
 
 
+
+
+def loginView(request):
+    context = {
+        "judul" : "DO IT",
+        "subjudul" : "SELAMAT DATANG DI DOIT"
+    }
+    user = None
+
+    if request.method == "POST":
+        username_login = request.POST['username']
+        password_login = request.POST['password']
+        print(username_login)
+        user = authenticate(request, username = username_login, password = password_login)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return redirect('login')
+
+    return render(request, 'login.html', context)
+
+
+
+def logoutView(request):
+    context = {
+            'judul' : "logout",
     }
 
-    return render(request, 'index.html', context)
+    if request.method == "POST":
+        if request.POST["logout"] == "Submit":
+            logout(request)
+
+        return redirect('login')
+
+    return render(request , 'logout.html', context)
+
+
+
+
+
+
+
+
+
+
